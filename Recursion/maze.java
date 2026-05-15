c5e9c67ca50ee8ba98dbe34f8df8e71376595980
Sarvesh9523 <sarveshkumar9176@gmail.com>
2026-05-15 13:04:48 +0530

    maze path finding algo

---
+++ Recursion/maze.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class maze {
+    public static void main(String[] args) {
+        System.out.println(countPath(3, 3));
+        // totalPath("", 3, 3);
+        System.out.println(totalPathList("", 3, 3));
+
+    }
+    static int countPath(int r , int c){
+    if(r == 1 || c == 1){
+    return 1 ;
+
+    }
+    int left = countPath(r-1, c);
+    int right = countPath(r, c-1);
+
+    return left + right ;
+
+    }
+
+    // now print the total path
+    static void totalPath(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            System.out.println(p);
+            return;
+        }
+        if (r > 1) {
+            totalPath(p + "D", r - 1, c);
+        }
+        if (c > 1) {
+            totalPath(p + "R", r, c - 1);
+        }
+
+    }
+
+    //Print using list
+    static ArrayList<String> totalPathList(String p, int r, int c){
+        if(r == 1 && c == 1){
+            ArrayList<String> paths = new ArrayList<>();
+            paths.add(p) ;
+            return paths ;
+        }
+        ArrayList<String> paths = new ArrayList<>();
+        if(r > 1){
+            paths.addAll(totalPathList(p + "D", r - 1, c));
+        }
+        if(c>1){
+            paths.addAll(totalPathList(p + "R", r, c-1));
+        }
+
+        return paths ;
+
+
+
+    }
+
+}
